13fed509f768b3d29424298685dcf681754fc061
harabat <45575009+harabat@users.noreply.github.com>
2021-12-04 14:05:54 +0100

    implement Home view

--- conduit/articles/views.py
+++ conduit/articles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Article
 
 
-def global_feed(request):
+def home(request):
     """all published articles"""
     articles = Article.objects.order_by("-created_at")
     return render(request, "home.html", {"articles": articles})
