07972354697d480983471b83f846ebd485ee9b85
soup <2-soup@users.noreply.gitlab.soupy-soup.org>
2021-12-04 04:53:52 +0100

    first view

--- conduit/articles/views.py
+++ conduit/articles/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from .models import Article
 
 
-# def global_feed(request):
-#     """all published articles"""
-#     articles = Article.objects.order_by("-created_at")
-#     # return render(request, "articles/global_feed.html", {"articles": articles})
-#     return render(request, "base.html")
+def global_feed(request):
+    """all published articles"""
+    articles = Article.objects.order_by("-created_at")
+    return render(request, "home.html", {"articles": articles})
