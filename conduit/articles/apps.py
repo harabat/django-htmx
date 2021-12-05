055432873d9659b29b630088814ab280c8337a94
harabat <45575009+harabat@users.noreply.github.com>
2021-12-05 18:40:27 +0100

    finish /Viewing Articles/ section

--- conduit/articles/apps.py
+++ conduit/articles/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ArticlesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "conduit.articles"
+
+    def ready(self):
+        import conduit.articles.signals
