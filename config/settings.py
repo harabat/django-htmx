c66df07a196eedcaa9b7c0f714cbc8ecf62340f5
harabat <45575009+harabat@users.noreply.github.com>
2021-12-15 14:01:41 +0100

    move config files to /config/, change tutorial

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "conduit.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "conduit.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
