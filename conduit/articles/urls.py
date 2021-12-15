00b012b14080c5665d44e016073b344bbba4c11c
harabat <45575009+harabat@users.noreply.github.com>
2021-12-15 02:08:26 +0100

    finish /Creating comments/ and /Deleting comments/

--- conduit/articles/urls.py
+++ conduit/articles/urls.py
@@ -5,18 +5,19 @@ from .views import (
     EditorCreateView,
     EditorUpdateView,
     EditorDeleteView,
-    CommentCreateView,
+    ArticleCommentView,
+    CommentDeleteView,
 )
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
-    path("article/<slug:slug>", ArticleDetailView.as_view(), name="article_detail"),
+    path("article/<slug:slug>", ArticleCommentView.as_view(), name="article_detail"),
     path("editor", EditorCreateView.as_view(), name="editor_create"),
     path("editor/<slug:slug>", EditorUpdateView.as_view(), name="editor_update"),
     path("editor/<slug:slug>/delete", EditorDeleteView.as_view(), name="editor_delete"),
     path(
-        "article/<slug:slug>/comment/",
-        CommentCreateView.as_view(),
-        name="comment_create",
+        "article/<slug:slug>/comment/<int:pk>/delete",
+        CommentDeleteView.as_view(),
+        name="comment_delete",
     ),
 ]
