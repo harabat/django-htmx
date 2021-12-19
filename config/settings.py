ffa4b787b853c65086d6259989cd0214c7b5500b
soup <2-soup@users.noreply.gitlab.soupy-soup.org>
2021-12-19 21:01:38 +0100

    implement user logic

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "conduit.articles",
+    "conduit.users",
 ]
+
+AUTH_USER_MODEL = "users.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
