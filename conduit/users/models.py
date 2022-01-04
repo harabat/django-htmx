47cbf75f02dd11b4be40d55d942bb9b8e07617fc
soup <2-soup@users.noreply.gitlab.soupy-soup.org>
2022-01-04 17:36:23 +0100

    implement login on signup

--- conduit/users/models.py
+++ conduit/users/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class CustomUserManager(UserManager):
     """custom UserManager with unique identifier is email instead of username"""
 
-    def create_user(self, username, email, password=None):
+    def create_user(self, email, username, password=None):
         """Create and return a User with username, email, and password"""
 
         if email is None:
@@ -15,21 +15,20 @@ class CustomUserManager(UserManager):
             raise ValueError("Username is required")
 
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email)
+        user = self.model(email=email, username=username)
         user.set_password(password)
         user.save()
+
         return user
 
-    def create_superuser(self, username, email, password=None):
+    def create_superuser(self, email, username, password=None):
         """Create and return a SuperUser with admin permissions."""
 
         user.is_staff = True
         user.is_superuser = True
         user.is_active = True
-
+        user = self.create_user(email, username, password)
         user.save()
-
-        user = self.create_user(username, email, password)
 
         return user
 
@@ -37,8 +36,8 @@ class CustomUserManager(UserManager):
 class User(AbstractUser):
     """User model"""
 
-    username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=255, unique=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
@@ -54,3 +53,4 @@ class Profile(models.Model):
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.URLField(blank=True)
+    bio = models.TextField(max_length=1000, blank=True)
--- conduit/users/views.py
+++ conduit/users/views.py
@@ -2,18 +2,26 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth import login
 from .models import User
 
 
 class Login(LoginView):
     template_name = "login.html"
+    next_page = reverse_lazy("home")
 
 
 class Logout(LogoutView):
-    template_name = "logout.html"
+    next_page = reverse_lazy("home")
 
 
 class SignUpView(CreateView):
     model = User
+    fields = ["username", "email", "password"]
     template_name = "signup.html"
     success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        login(self.request, self.object)
+        return valid
