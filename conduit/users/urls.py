70142984d3ad9f44e1f72168f764704a9cf023df
soup <2-soup@users.noreply.gitlab.soupy-soup.org>
2022-01-06 00:24:57 +0100

    implement /Viewing Profiles/ section

--- conduit/users/urls.py
+++ conduit/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Login, Logout, SignUpView
+from .views import Login, Logout, SignUpView, ProfileDetailView
 
 
 urlpatterns = [
     path("login", Login.as_view(), name="login"),
     path("logout", Logout.as_view(), name="logout"),
     path("signup", SignUpView.as_view(), name="signup"),
+    path("profile/@<str:username>", ProfileDetailView.as_view(), name="profile_detail"),
 ]
