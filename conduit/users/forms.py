21325b52cb3def71ce3fa91f3538fbf9594ea448
harabat <45575009+harabat@users.noreply.github.com>
2022-01-09 22:55:28 +0100

    finish /Editing Profiles/

--- conduit/users/forms.py
+++ conduit/users/forms.py
@@ -9,6 +9,16 @@ class ProfileForm(forms.ModelForm):
 
 
 class UserForm(forms.ModelForm):
+    new_password = forms.CharField(required=False)
+
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = ["username", "email", "new_password"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        new_password = self.cleaned_data.get("new_password")
+        if new_password:
+            user.set_password(new_password)
+        user.save()
+        return user
